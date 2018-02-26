cab4cd2ecdb9280ebba35fb719b4bed1145b95a2
MoHo Khaleqi <habibsandyfan@gmail.com>
2018-02-26 20:34:23 +0000

    Handle CORS erro

--- app/app.js
+++ app/app.js
@@ -12,6 +12,18 @@ app.use(morgan('dev'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use((req, res, next) => {
+    // Prevent CORS erros
+    // It must come before routes
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Typr, Accept, Authorization')
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, GET, DELETE')
+        return res.status(200).json({})
+    }
+    next()
+})
+
 // Use routes
 app.use('/products', productsRoute);
 app.use('/orders', ordersRoute);
