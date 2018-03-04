afb6e002641bc916d3e67bcc237bd10d4ed1fc02
MoHo Khaleqi <habibsandyfan@gmail.com>
2018-03-04 23:30:15 +0000

    Add auth middleware for delete user

--- app/api/routes/user.js
+++ app/api/routes/user.js
@@ -1,9 +1,10 @@
 import express from 'express'
 const router = express.Router()
 import UserController from './../controllers/user'
+import auth from './../middlewares/auth'
 
 router.post('/signup', UserController.create)
 router.post('/login', UserController.login)
-router.delete('/:userId', UserController.delete)
+router.delete('/:userId', auth, UserController.delete)
 
 module.exports = router
