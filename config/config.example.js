304a73980f0be75656bbedd9d608251064507655
MoHo Khaleqi <habibsandyfan@gmail.com>
2018-03-08 13:00:25 +0000

    Update .gitignore

---
+++ config/config.example.js
@@ -0,0 +1,10 @@
+let config = {
+    "port": 3000,
+    "url": "http://localhost",
+    "db": {
+        "mongodbLinkMlab": "mongodb://username:password@ds247698.mlab.com:47698/node-restfull-api"
+    },
+    "JWT_KEY" : "secret"
+}
+
+module.exports = config
